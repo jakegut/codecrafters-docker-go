1cb11d98cbd3a2dd1b206f280664d8d27674931a
Jake Gutierrez <jake.gutierrez@clever.com>
2022-11-18 10:23:17 -0600

    .

--- app/main.go
+++ app/main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 	// Uncomment this block to pass the first stage!
 	// "os"
 	// "os/exec"
 )
+
+func handleError(e error) {
+	if e != nil {
+		log.Fatalf("%+v", e)
+	}
+}
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
@@ -16,11 +27,20 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	tmpChroot, err := ioutil.TempDir("", "")
+	handleError(err)
+
+	handleError(copyExecToDir(tmpChroot, command))
+
+	handleError(createDevNull(tmpChroot))
+
+	handleError(syscall.Chroot(tmpChroot))
+
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		fmt.Printf("Err: %+v", err)
 		var exitError *exec.ExitError
@@ -30,4 +50,44 @@ func main() {
 			os.Exit(1)
 		}
 	}
+}
+
+func copyExecToDir(chootDir, execPath string) error {
+	execPathInDir := path.Join(chootDir, execPath)
+
+	if err := os.MkdirAll(path.Dir(execPathInDir), 0750); err != nil {
+		return err
+	}
+
+	return copyFile(execPath, execPathInDir)
+}
+
+func copyFile(src, dest string) error {
+	srcStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE, srcStat.Mode())
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	return err
+}
+
+func createDevNull(chroot string) error {
+	if err := os.MkdirAll(path.Join(chroot, "dev"), 0750); err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path.Join(chroot, "dev", "null"), []byte{}, 0644)
 }
