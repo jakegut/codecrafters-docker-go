9744e283f522b0905ccf8c479df2e6f501e5b5ca
Jake Gutierrez <jake.gutierrez@clever.com>
2022-11-18 09:56:33 -0600

    .

--- app/main.go
+++ app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	// Uncomment this block to pass the first stage!
 	// "os"
 	// "os/exec"
@@ -12,17 +14,15 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage!
-	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Println(string(output))
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(output))
 }
